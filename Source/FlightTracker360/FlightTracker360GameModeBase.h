c17dc9b76510dfaa83927eed8a316482aad74431
Heydar Mahmoodi <heydar.mahmoodi75@gmail.com>
2022-11-06 06:10:14 +0330

    default project has created without starter contents and sky-light too

---
+++ Source/FlightTracker360/FlightTracker360GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FlightTracker360GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FLIGHTTRACKER360_API AFlightTracker360GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
